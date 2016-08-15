c48a4b54a069ae82604be0a62d94e3906e9755d1
jonathanhinton <hintonjonathan@gmail.com>
2016-08-14 22:29:53 -0500

    add angular controller and basic functionality to grab string from input

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('linkValidator', ['ngAnimate', 'ngAria', 'ngRoute', 'ngMaterial']);
+
+app.controller('linkCtrl',
+  [
+    '$http',
+    '$q',
+    function($http, $q){
+
+      var myUrl = "";
+
+      this.printURL = function(){
+        myUrl = this.SearchURL;
+        console.log("this is what you typed", myUrl);
+      };
+
+  }]);
