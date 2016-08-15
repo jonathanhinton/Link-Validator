1e1a3f786529a5f8beacc789ccdd7e1774374cab
jonathanhinton <hintonjonathan@gmail.com>
2016-08-14 23:12:56 -0500

    add success and error functions for promise; comment on current SOP issue

--- app/app.js
+++ app/app.js
@@ -14,10 +14,21 @@ app.controller('linkCtrl',
         myUrl = this.SearchURL;
         console.log("this is what you typed", myUrl);
 
-        //make xhr via Angular's $http.get using myUrl as url
-        $http.get(myUrl).then(function(response){
+        function handleError(response){
+          if (!angular.isObject(response.data) || !response.data.message)
+          {
+            return ( $q.reject("an unknown error occured"));
+          } else {
+            return ($q.reject(response.data.message));
+          }
+        }
+
+        function handleSuccess(response){
           console.log("response", response);
-        });
+        }
+
+        //make xhr via Angular's $http.get using myUrl as url...and here is where my trouble begins.  I have been unsuccessful in getting responses from most sites due to Access-Control-Allow-Origin header and Single Origin Policy.  I have been looking for a CORS workaround but have been spinning my wheels thus far.
+        $http.get(myUrl).then(handleSuccess, handleError);
 
 
       };
