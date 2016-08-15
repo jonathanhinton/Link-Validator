7f6e70ff0e17b347a1178131226ca54748ae1952
jonathanhinton <hintonjonathan@gmail.com>
2016-08-14 22:49:44 -0500

    begin setup for making xhr using

--- app/app.js
+++ app/app.js
@@ -9,9 +9,15 @@ app.controller('linkCtrl',
       var myUrl = "";
 
       this.printURL = function(){
+
         //assign myUrl variable the value of what was typed into the input
         myUrl = this.SearchURL;
         console.log("this is what you typed", myUrl);
+
+        //make xhr via Angular's $http.get using myUrl as url
+        $http.get(myUrl).then(function(response){
+          console.log("response", response);
+        });
 
 
       };
