14b961ea6a1cf429050b96f003f8aa12636c5e61
jonathanhinton <hintonjonathan@gmail.com>
2016-08-14 22:40:19 -0500

    begin commenting app.js for future understandablity

--- app/app.js
+++ app/app.js
@@ -9,8 +9,11 @@ app.controller('linkCtrl',
       var myUrl = "";
 
       this.printURL = function(){
+        //assign myUrl variable the value of what was typed into the input
         myUrl = this.SearchURL;
         console.log("this is what you typed", myUrl);
+
+
       };
 
   }]);
